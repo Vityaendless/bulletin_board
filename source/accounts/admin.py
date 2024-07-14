dfb6c8759b40fd4a9644a7609144bf4bb9bafff1
Vitya Endless <dvitya031190@gmail.com>
2024-07-14 14:43:12 +0600

    add user model

--- source/accounts/admin.py
+++ source/accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import AppUser
+
+
+@admin.register(AppUser)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username', 'email', 'phone']
+    list_display_links = ['id', 'username', 'email']
+    list_filter = ['id', 'username', 'email', 'phone']
+    search_fields = ['username', 'email', 'phone']
+    fields = [
+        'username', 'avatar', 'password', 'email', 'first_name',
+        'last_name', 'phone', 'ads_count', 'comments_count'
+    ]
--- source/accounts/models.py
+++ source/accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.shortcuts import reverse
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class AppUser(AbstractUser):
+    email = models.EmailField(max_length=30, unique=True, null=False, blank=False, verbose_name="Email")
+    avatar = models.ImageField(upload_to='user_pics', verbose_name='Avatar')
+    phone = models.CharField(max_length=15, null=True, blank=True, verbose_name="Phone number")
+    ads_count = models.IntegerField(default=0, verbose_name='Ads count')
+    comments_count = models.IntegerField(default=0, verbose_name='Comments count')
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}, username: {self.username}'
+
+    # def get_absolute_url(self):
+    #     return reverse('accounts:profile', kwargs={'pk': self.pk})
