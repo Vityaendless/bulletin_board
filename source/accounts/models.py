274f7d32130470cb6b0c319d66ebb541dc0b01ba
Vitya Endless <dvitya031190@gmail.com>
2024-07-24 12:22:11 +0600

    add mask for phone

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -1,17 +1,15 @@
 from django.db import models
 from django.shortcuts import reverse
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinLengthValidator
 
 
 class AppUser(AbstractUser):
     email = models.EmailField(max_length=30, unique=True, null=False, blank=False, verbose_name="Email")
     avatar = models.ImageField(upload_to='user_pics', verbose_name='Avatar')
-    phone = models.CharField(max_length=15, null=True, blank=True, verbose_name="Phone number")
+    phone = models.CharField(max_length=16, validators=[MinLengthValidator(16)], null=False, blank=False, verbose_name="Phone number")
     ads_count = models.IntegerField(default=0, verbose_name='Ads count')
     comments_count = models.IntegerField(default=0, verbose_name='Comments count')
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}, username: {self.username}'
-
-    # def get_absolute_url(self):
-    #     return reverse('accounts:profile', kwargs={'pk': self.pk})
